80551276bc30d5cefc04efa05c381cf504d3e235
Yash Koladiya <yashkoladiya5@gmail.com>
2026-07-25 18:01:36 +0530

    document dependency injection setup

--- flutter_app/lib/core/di/injection.dart
+++ flutter_app/lib/core/di/injection.dart
@@ -2,7 +2,9 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'injection.config.dart';
 
+/// Global instance of GetIt for dependency injection.
 final getIt = GetIt.instance;
 
+/// Configures and initializes all generated dependencies.
 @InjectableInit()
 void configureDependencies() => getIt.init();
--- flutter_app/lib/core/di/register_module.dart
+++ flutter_app/lib/core/di/register_module.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
 
+/// Module for registering third-party dependencies with Injectable.
 @module
 abstract class RegisterModule {
+  /// Provides a singleton instance of FlutterSecureStorage for secure local storage.
   @lazySingleton
   FlutterSecureStorage get secureStorage => const FlutterSecureStorage();
 }
