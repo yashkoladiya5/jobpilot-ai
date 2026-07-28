0f0eaba77779a5e0d3ef5dcb9a6376310967007b
Yash Koladiya <yashkoladiya5@gmail.com>
2026-07-28 05:10:35 +0000

    add descriptions to dashboard and analytics interfaces

--- flutter_app/lib/domain/repositories/analytics_repository.dart
+++ flutter_app/lib/domain/repositories/analytics_repository.dart
@@ -2,6 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:jobpilot_ai/core/errors/failures.dart';
 import 'package:jobpilot_ai/domain/entities/pipeline_analytics.dart';
 
+/// Abstract definition of the Analytics Repository.
+/// Provides metrics on application pipeline and historical timeline data.
 abstract class AnalyticsRepository {
   Future<Either<Failure, PipelineAnalytics>> getPipelineAnalytics();
   Future<Either<Failure, List<TimelineEntry>>> getTimelineData();
--- flutter_app/lib/domain/repositories/dashboard_repository.dart
+++ flutter_app/lib/domain/repositories/dashboard_repository.dart
@@ -2,6 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:jobpilot_ai/core/errors/failures.dart';
 import 'package:jobpilot_ai/domain/entities/dashboard_stats.dart';
 
+/// Abstract definition of the Dashboard Repository.
+/// Fetches aggregated statistics for the user's home screen.
 abstract class DashboardRepository {
   Future<Either<Failure, DashboardStats>> getStats();
 }
