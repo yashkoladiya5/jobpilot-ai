3e64e73f894eb5d32fc02b1097eed9a66d9fe864
Yash Koladiya <yashkoladiya5@gmail.com>
2026-09-11 11:57:14 +0530

    Sort import directives in main entrypoint

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jobpilot_ai/app.dart';
 import 'package:jobpilot_ai/core/di/injection.dart';
 import 'package:jobpilot_ai/presentation/bloc/app_bloc_observer.dart';
